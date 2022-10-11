073c1ec9c504859461ab6b7b51abf6d2714c0487
Leonardo M Carvalho <104258969+mercesleonardo@users.noreply.github.com>
2022-10-11 09:31:56 -0300

    Criando O primeiro Controller

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,11 @@
 <?php
 
+use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', [SiteController::class, 'index']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     //return view('welcome');
+//     return 'Ainda estou no routers';
+// });

86b937d9dfc20b2d127f76cdd55f9a009b38a6d7
Leonardo M Carvalho <104258969+mercesleonardo@users.noreply.github.com>
2022-10-11 11:31:49 -0300

    Condicionais

--- resources/views/bemvindo.blade.php
+++ resources/views/bemvindo.blade.php
@@ -7,10 +7,21 @@
     <title>Bem-vindo</title>
 </head>
 <body>
+    <!--
     <h1>Bem-vindo, {{$apelido_nome}}</h1>
     <h2> 1 + 1 é: {{1+1}}</h2>
     <h3>O código é: {{--funcao()--}}</h3>
     <p>Código HTML: {{$html}}</p>
     <p>Código HTML interpretado: {!!$html!!}</p>
+    -->
+    <h1>{{$apelido_nome == 'Jessica' ? 'Sim': 'Não'}} </h1>
+
+    <span>O usuário é {{$apelido_nome}}?</span>
+    @if($apelido_nome == 'Leonardo')
+        <h1>Sim</h1>
+    @else
+        <h1>Não</h1>
+    @endif
+
 </body>
 </html>

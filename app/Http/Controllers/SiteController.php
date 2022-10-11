5c7dc37b37e7d8dd71ccce3ddb39786f802a3537
Leonardo M Carvalho <104258969+mercesleonardo@users.noreply.github.com>
2022-10-11 10:06:24 -0300

    Passando dados para a View

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -11,7 +11,20 @@ class SiteController extends Controller
         // Criar uma lógica
         // Verificar se um usuario existe
         // Buscar dados de um usuário, etc...
-        return view('bemvindo');
+
+        $name = 'Leonardo';
+        $sobrenome = 'Carvalho';
+        $age = 36;
+        $aniversario = '19-07-1986';
+
+        $data = [
+            'apelido_nome' => $name,
+            'apelido_sobrenome' => $sobrenome,
+            'idade' => $age,
+            'nascimento' => $aniversario
+        ];
+
+        return view('bemvindo', $data);
     }
 
     public function exit() {
--- resources/views/bemvindo.blade.php
+++ resources/views/bemvindo.blade.php
@@ -1,1 +1,14 @@
-Bem vindo!!
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bem-vindo</title>
+</head>
+<body>
+    <h1>Bem-vindo, {{$apelido_nome}} {{$apelido_sobrenome}}</h1>
+    <h2> Idade: {{$idade}} anos</h2>
+    <h2>Nascimento: {{$nascimento}}</h2>
+</body>
+</html>
